37495e4a45feb1c83eabbca18783a75d15ecea1f
dannyroes <danny.roes@gmail.com>
2021-05-12 11:04:17 -0400

    Tuple and basic functions

--- tuple.go
+++ tuple.go
@@ -1,8 +1,58 @@
 package main
 
+import "math"
+
+var Epsilon = math.Nextafter(1.0, 2.0) - 1.0
+
 type Tuple struct {
 	X float64
 	Y float64
 	Z float64
 	W float64
+}
+
+func (t Tuple) IsPoint() bool {
+	return FloatEqual(1.0, t.W)
+}
+
+func (t Tuple) IsVector() bool {
+	return FloatEqual(0.0, t.W)
+}
+
+func (t Tuple) Add(b Tuple) Tuple {
+	t.X += b.X
+	t.Y += b.Y
+	t.Z += b.Z
+	t.W += b.W
+
+	return t
+}
+
+func FloatEqual(x, y float64) bool {
+	return math.Abs(x-y) < Epsilon
+}
+
+func Point(x, y, z float64) Tuple {
+	return Tuple{
+		X: x,
+		Y: y,
+		Z: z,
+		W: 1.0,
+	}
+}
+
+func Vector(x, y, z float64) Tuple {
+	return Tuple{
+		X: x,
+		Y: y,
+		Z: z,
+		W: 0.0,
+	}
+}
+
+func TupleEqual(a, b Tuple) bool {
+	return FloatEqual(a.X, b.X) &&
+		FloatEqual(a.Y, b.Y) &&
+		FloatEqual(a.Z, b.Z) &&
+		FloatEqual(a.W, b.W)
 }
--- tuple_test.go
+++ tuple_test.go
@@ -2,15 +2,142 @@ package main
 
 import "testing"
 
+func TestFloatEqual(t *testing.T) {
+	a := 0.1
+	a += 0.2
+	a += 0.2
+
+	b := 0.5
+
+	if !FloatEqual(a, b) {
+		t.Error("Expected true")
+	}
+
+	a += 0.01
+
+	if FloatEqual(a, b) {
+		t.Error("Expected false")
+	}
+}
+
 func TestPointTuple(t *testing.T) {
-	// a := Tuple{
-	// 	X: 4.3,
-	// 	Y: -4.2,
-	// 	Z: 3.1,
-	// 	W: 1.0,
-	// }
+	a := Tuple{
+		X: 4.3,
+		Y: -4.2,
+		Z: 3.1,
+		W: 1.0,
+	}
+
+	if !FloatEqual(a.X, 4.3) {
+		t.Error("X value incorrect")
+	}
+
+	if !FloatEqual(a.Y, -4.2) {
+		t.Error("Y value incorrect")
+	}
+
+	if !FloatEqual(a.Z, 3.1) {
+		t.Error("Z value incorrect")
+	}
+
+	if !FloatEqual(a.W, 1.0) {
+		t.Error("W value incorrect")
+	}
+
+	if !a.IsPoint() || a.IsVector() {
+		t.Error("Point reported as vector")
+	}
 }
 
 func TestVectorTuple(t *testing.T) {
+	a := Tuple{
+		X: 4.3,
+		Y: -4.2,
+		Z: 3.1,
+		W: 0.0,
+	}
+
+	if !FloatEqual(a.X, 4.3) {
+		t.Error("X value incorrect")
+	}
+
+	if !FloatEqual(a.Y, -4.2) {
+		t.Error("Y value incorrect")
+	}
+
+	if !FloatEqual(a.Z, 3.1) {
+		t.Error("Z value incorrect")
+	}
+
+	if !FloatEqual(a.W, 0.0) {
+		t.Error("W value incorrect")
+	}
+
+	if a.IsPoint() || !a.IsVector() {
+		t.Error("Point reported as vector")
+	}
+}
+
+func TestTupleEqual(t *testing.T) {
+	a := Tuple{
+		X: 4.3,
+		Y: -4.2,
+		Z: 3.1,
+		W: 0.0,
+	}
+
+	b := a
+
+	if !TupleEqual(a, b) {
+		t.Error("Expected equal")
+	}
+
+	b.X += 0.2
+
+	if TupleEqual(a, b) {
+		t.Error("Expected unequal")
+	}
+}
+
+func TestPointFactory(t *testing.T) {
+	a := Tuple{
+		X: 4,
+		Y: -4,
+		Z: 3,
+		W: 1.0,
+	}
+
+	b := Point(4, -4, 3)
+
+	if !TupleEqual(a, b) {
+		t.Error("Expected equal")
+	}
+}
+
+func TestVectorFactory(t *testing.T) {
+	a := Tuple{
+		X: 4,
+		Y: -4,
+		Z: 3,
+		W: 0,
+	}
+
+	b := Vector(4, -4, 3)
+
+	if !TupleEqual(a, b) {
+		t.Error("Expected equal")
+	}
+}
+
+func TestAddTuple(t *testing.T) {
+	a := Point(3, -2, 5)
+	b := Vector(-2, 3, 1)
+
+	c := a.Add(b)
+	expected := Point(1, 1, 6)
+
+	if !TupleEqual(c, expected) {
+		t.Errorf("Expected %+v, received %+v", expected, c)
+	}
 
 }
