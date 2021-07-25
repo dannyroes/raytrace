7041047e38ece1070adfc16fa4b4858c09d2fd8d
dannyroes <danny.roes@gmail.com>
2021-07-25 13:17:32 -0400

    Scene with patterns

--- main.go
+++ main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"math"
 	"os"
 	"strconv"
-	"time"
 )
 
 type Environment struct {
@@ -45,6 +43,7 @@ func drawScene(width, height, supersample int) {
 	floor := Plane()
 	m := Material()
 	m.Colour = Colour(1, 0.9, 0.9)
+	m.Pattern = CheckersPattern(Colour(1, 0.9, 0.9), Colour(1, 0.1, 0.1))
 	m.Specular = 0
 	floor.SetMaterial(m)
 
@@ -55,10 +54,6 @@ func drawScene(width, height, supersample int) {
 	rightWall := Plane()
 	rightWall.Transform = RotateX(math.Pi/2).RotateY(math.Pi/4).Translate(0, 0, 26)
 	rightWall.SetMaterial(m)
-
-	ceiling := Plane()
-	ceiling.Transform = Translation(0, 6, 0)
-	ceiling.SetMaterial(m)
 
 	middle := Sphere()
 	middle.Transform = Translation(0, 1, -0.5)
@@ -101,7 +96,6 @@ func drawScene(width, height, supersample int) {
 		floor,
 		leftWall,
 		rightWall,
-		ceiling,
 		middle,
 		left,
 		right,
@@ -114,113 +108,113 @@ func drawScene(width, height, supersample int) {
 	}
 }
 
-func drawCircle() {
-	canvas := Canvas(500, 500)
-	colour := Colour(1, 0.5, 0.3)
-	sphere := Sphere()
-	sphere.SetTransform(Scaling(200, 100, 200).RotateZ(2.5).Translate(250, 250, 250))
+// func drawCircle() {
+// 	canvas := Canvas(500, 500)
+// 	colour := Colour(1, 0.5, 0.3)
+// 	sphere := Sphere()
+// 	sphere.SetTransform(Scaling(200, 100, 200).RotateZ(2.5).Translate(250, 250, 250))
 
-	for x := 0; x < canvas.Width; x++ {
-		for y := 0; y < canvas.Height; y++ {
-			ray := Ray(Point(float64(x), float64(y), -500), Vector(0, 0, 1))
-			h := Intersects(sphere, ray).Hit()
+// 	for x := 0; x < canvas.Width; x++ {
+// 		for y := 0; y < canvas.Height; y++ {
+// 			ray := Ray(Point(float64(x), float64(y), -500), Vector(0, 0, 1))
+// 			h := Intersects(sphere, ray).Hit()
 
-			if h.Object == sphere {
-				canvas.WritePixel(x, y, colour)
-			}
-		}
-	}
+// 			if h.Object == sphere {
+// 				canvas.WritePixel(x, y, colour)
+// 			}
+// 		}
+// 	}
 
-	ioutil.WriteFile("proj.ppm", []byte(canvas.ToPPM()), 0755)
-}
+// 	ioutil.WriteFile("proj.ppm", []byte(canvas.ToPPM()), 0755)
+// }
 
-func drawCircleSingleOrigin() {
-	canvas := Canvas(500, 500)
-	wallZ := 200.0
-	rayOrigin := Point(0, 0, -20)
-	sphere := Sphere()
-	sphere.SetTransform(IdentityMatrix().Scale(10, 10, 10))
-	sphere.Material.Colour = Colour(1, 0.2, 1)
-	sphere.Material.Shininess = 50
+// func drawCircleSingleOrigin() {
+// 	canvas := Canvas(500, 500)
+// 	wallZ := 200.0
+// 	rayOrigin := Point(0, 0, -20)
+// 	sphere := Sphere()
+// 	sphere.SetTransform(IdentityMatrix().Scale(10, 10, 10))
+// 	sphere.Material.Colour = Colour(1, 0.2, 1)
+// 	sphere.Material.Shininess = 50
 
-	light := PointLight(Point(-10, 10, -10), Colour(1, 1, 1))
+// 	light := PointLight(Point(-10, 10, -10), Colour(1, 1, 1))
 
-	start := time.Now()
+// 	start := time.Now()
 
-	for x := -250; x < 250; x++ {
-		for y := -250; y < 250; y++ {
-			v := Point(float64(x), float64(y), wallZ).Sub(rayOrigin).Normalize()
-			ray := Ray(rayOrigin, v)
-			h := Intersects(sphere, ray).Hit()
+// 	for x := -250; x < 250; x++ {
+// 		for y := -250; y < 250; y++ {
+// 			v := Point(float64(x), float64(y), wallZ).Sub(rayOrigin).Normalize()
+// 			ray := Ray(rayOrigin, v)
+// 			h := Intersects(sphere, ray).Hit()
 
-			if h.T >= 0 {
-				p := ray.Position(h.T)
-				normal := NormalAt(h.Object, p)
-				eye := ray.Direction.Neg()
-				colour := Lighting(h.Object.GetMaterial(), light, p, eye, normal, false)
-				canvas.WritePixel(x+250, y+250, colour)
-			}
-		}
-	}
+// 			if h.T >= 0 {
+// 				p := ray.Position(h.T)
+// 				normal := NormalAt(h.Object, p)
+// 				eye := ray.Direction.Neg()
+// 				colour := Lighting(h.Object.GetMaterial(), light, p, eye, normal, false)
+// 				canvas.WritePixel(x+250, y+250, colour)
+// 			}
+// 		}
+// 	}
 
-	elapsed := time.Since(start)
-	fmt.Println(elapsed)
+// 	elapsed := time.Since(start)
+// 	fmt.Println(elapsed)
 
-	ioutil.WriteFile("output/proj.ppm", []byte(canvas.ToPPM()), 0755)
-}
+// 	ioutil.WriteFile("output/proj.ppm", []byte(canvas.ToPPM()), 0755)
+// }
 
-func drawClock() {
-	canvas := Canvas(500, 500)
+// func drawClock() {
+// 	canvas := Canvas(500, 500)
 
-	rad := math.Pi * 2
+// 	rad := math.Pi * 2
 
-	start := Point(0, 225, 0)
-	colour := Colour(1, 1, 1)
+// 	start := Point(0, 225, 0)
+// 	colour := Colour(1, 1, 1)
 
-	for x := 0; x < 12; x++ {
-		point := IdentityMatrix().RotateZ(rad/12*float64(x)).Translate(250, 250, 0).MultiplyTuple(start)
+// 	for x := 0; x < 12; x++ {
+// 		point := IdentityMatrix().RotateZ(rad/12*float64(x)).Translate(250, 250, 0).MultiplyTuple(start)
 
-		fmt.Println(point)
-		canvas.WritePixel(int(point.X), int(point.Y), colour)
-	}
+// 		fmt.Println(point)
+// 		canvas.WritePixel(int(point.X), int(point.Y), colour)
+// 	}
 
-	ioutil.WriteFile("proj.ppm", []byte(canvas.ToPPM()), 0755)
-}
+// 	ioutil.WriteFile("proj.ppm", []byte(canvas.ToPPM()), 0755)
+// }
 
-func runSim() {
-	var canvasHeight, canvasWidth int
-	canvasHeight = 550
-	canvasWidth = 900
-	canvas := Canvas(canvasWidth, canvasHeight)
+// func runSim() {
+// 	var canvasHeight, canvasWidth int
+// 	canvasHeight = 550
+// 	canvasWidth = 900
+// 	canvas := Canvas(canvasWidth, canvasHeight)
 
-	projColour := Colour(1, 0, 0)
-	e := Environment{Gravity: Vector(0, -0.08, 0), Wind: Vector(-0.05, 0, 0)}
+// 	projColour := Colour(1, 0, 0)
+// 	e := Environment{Gravity: Vector(0, -0.08, 0), Wind: Vector(-0.05, 0, 0)}
 
-	p := Projectile{Position: Point(0, 1, 0), Velocity: Vector(1.4, 1.8, 0).Normalize().Mul(11.25)}
-	ticks := 0
-	for {
-		posX := int(p.Position.X)
-		posY := canvasHeight - int(p.Position.Y)
+// 	p := Projectile{Position: Point(0, 1, 0), Velocity: Vector(1.4, 1.8, 0).Normalize().Mul(11.25)}
+// 	ticks := 0
+// 	for {
+// 		posX := int(p.Position.X)
+// 		posY := canvasHeight - int(p.Position.Y)
 
-		if posX > canvasWidth || posX < 0 || posY > canvasHeight || posY < 0 {
-			break
-		}
-		canvas.WritePixel(posX, posY, projColour)
+// 		if posX > canvasWidth || posX < 0 || posY > canvasHeight || posY < 0 {
+// 			break
+// 		}
+// 		canvas.WritePixel(posX, posY, projColour)
 
-		if p.Position.Y < 0.0 || FloatEqual(p.Position.Y, 0) {
-			break
-		}
-		ticks++
-		p = tick(e, p)
-	}
+// 		if p.Position.Y < 0.0 || FloatEqual(p.Position.Y, 0) {
+// 			break
+// 		}
+// 		ticks++
+// 		p = tick(e, p)
+// 	}
 
-	ioutil.WriteFile("proj.ppm", []byte(canvas.ToPPM()), 0755)
+// 	ioutil.WriteFile("proj.ppm", []byte(canvas.ToPPM()), 0755)
 
-	fmt.Printf("Completed after %d ticks", ticks)
-}
+// 	fmt.Printf("Completed after %d ticks", ticks)
+// }
 
-func tick(e Environment, p Projectile) Projectile {
-	pos := p.Position.Add(p.Velocity)
-	vel := p.Velocity.Add(e.Gravity).Add(e.Wind)
-	return Projectile{Position: pos, Velocity: vel}
-}
+// func tick(e Environment, p Projectile) Projectile {
+// 	pos := p.Position.Add(p.Velocity)
+// 	vel := p.Velocity.Add(e.Gravity).Add(e.Wind)
+// 	return Projectile{Position: pos, Velocity: vel}
+// }
